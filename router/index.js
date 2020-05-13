7a393692e1034b0f890c1a153d8ade29927a9859
Stevie <yzm629@sina.cn>
2020-05-13 14:11:49 +0800

    update

--- router/index.js
+++ router/index.js
@@ -7,15 +7,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  //   component: require('../views/About.vue').default
-  // }
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const routerHistory = createWebHistory()
--- views/About.vue
+++ views/About.vue
@@ -11,7 +11,6 @@ export default {
 		}
 	},
 	mounted(){
-		console.log(this.$store.state.count)
 	}
 }
 </script>
