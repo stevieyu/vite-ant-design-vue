6bc70ea78bda1bde10f77facf1b4780dc5182b6d
Stevie <yzm629@sina.cn>
2020-11-05 11:27:01 +0800

    feat: add git hook

---
+++ pkg/githook/.commitlintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    // https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+    '@commitlint/config-conventional',
+  ],
+};
