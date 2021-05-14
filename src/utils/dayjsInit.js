0ba2442cd7e63669e279dbf0912d177afe3ffde3
Stevie <yzm629@sina.cn>
2021-05-14 12:13:46 +0800

    build: update package

--- src/utils/dayjsInit.js
+++ src/utils/dayjsInit.js
@@ -3,6 +3,8 @@ import {extend} from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import localeData from 'dayjs/plugin/localeData'
 import badMutable from 'dayjs/plugin/badMutable'
+import isMoment from 'dayjs/plugin/isMoment'
 
 extend(localeData)
 extend(badMutable)
+extend(isMoment)
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,18 +3,25 @@
     <div class="flex items-center">
       <a-button>sdfsdfsdf</a-button>
       <ACalendar :fullscreen="false"/>
+      <a-date-picker v-model:value="date"/>
     </div>
     <h1>This is an about page</h1>
   </div>
 </template>
 <script lang="ts">
 import {defineComponent} from 'vue'
-import {Calendar} from 'ant-design-vue';
-
+import {Calendar, DatePicker} from 'ant-design-vue';
+import dayjs from 'dayjs'
 
 export default defineComponent({
   components: {
-    [Calendar.name]: Calendar
+    [Calendar.name]: Calendar,
+    [DatePicker.name]: DatePicker
+  },
+  data(){
+    return {
+      date: dayjs('2020-01-12')
+    }
   }
 })
 </script>
